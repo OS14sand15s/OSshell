437040f31470e5bf6e0eb74c463dae528acbb50b
AlsikeE <AlsikeE@users.noreply.github.com>
2015-04-17 23:03:18 +0800

    Update execute.c
    加上了ctrlc 乃们看要不要在作业链表里删了当前作业呀

--- execute.c
+++ execute.c
@@ -184,6 +184,41 @@ void ctrl_Z(){
     fgPid = 0;
 }
 
+/*组合键命令ctrl+c*/
+void ctrl_C(){
+	Job *now = NULL;
+    //前台没有作业直接返回
+	if(fgPid == 0){
+	    return;
+	}
+    //SIGNCHLD信号产生自此函数
+	ingnore = 1;
+
+	now = head;
+	while (now != NULL && now -> pid != fgpid)
+		now = now -> next;
+
+	if (now == NULL){
+		now = addJob(fgPid);    //没有前台作业则根据fgPid 添加
+	}
+
+	strcpy(now -> state, KILLED);
+	now -> cmd[strlen(now -> cmd)] = '&';
+	now -> cmd[strlen(now -> cmd) + 1] = '\0';
+	printf("[%d]\t%s\t\t%s\n",now -> pid, now -> state, now -> cmd);
+
+	kill (fgPid, SIGKILL);             //sigstop -> sigkill
+	fgPid = 0;
+
+	//需要删除在作业链表中的当前作业
+	if (now == head ){
+		head = now->next;
+	}
+	else
+		last -> next =now ->next;
+	free(now);
+}
+
 /*fg命令*/
 void fg_exec(int pid){
     Job *now = NULL;
